154894a528567f3b46f0e65043fbbe96c2cacfa3
Ravindranath <ravindranath4363@gmail.com>
2022-05-25 14:53:48 +0530

    com not1

---
+++ app/src/main/java/com/example/tryfib1/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.firebasenotifications
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAXDzwQKc:APA91bGMkBPZfsOUSNrclA_Za4ZmsgTnK3LxaGZth-cewQvBQ_jryLvIFZYsXHbMnsfqRjAaxIOMaMo5E02YRRFuMjf4MNl7_W1PM2bswLqm_VzbXq1dgLKIcBTK85dco0FSFqDWa1zn"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
